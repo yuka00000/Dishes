1f19e85c18c1d8d8569675e7c81b856602b850c4
yuka00000 <yukayamada0914@gmail.com>
2023-10-06 06:16:26 +0000

    [Add]devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
