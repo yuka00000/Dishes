54e6fdd8ec249a851baf1066d13e4ed82d8b3027
yuka00000 <yukayamada0914@gmail.com>
2023-11-28 07:07:18 +0000

    フォロー、フォロワー画面修正

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -37,14 +37,14 @@ before_action :authenticate_user!
 
   # フォロー一覧
   def follows
-  user = User.find(params[:id])
-  @users = user.following_users
+  @user = User.find(params[:id])
+  @users = @user.following_users
   end
 
   # フォロワー一覧
   def followers
-    user = User.find(params[:id])
-    @user = user.follower_users
+    @user = User.find(params[:id])
+    @users = @user.follower_users
   end
 
   private
